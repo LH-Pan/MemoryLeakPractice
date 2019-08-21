cebf94169042239e4361a4c42170297596c7c7cd
潘立祥 <panlishiang@panlixiangde-MacBook-Pro.local>
2019-08-21 14:36:19 +0800

    create memory leak project

---
+++ MemoryLeakPractice/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MemoryLeakPractice
+//
+//  Created by 潘立祥 on 2019/8/21.
+//  Copyright © 2019 PanLiHsiang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
